54ae233421d77d85eb3630463531c0775a12ec95
Moeenuddin Admad <mashruf125@gmail.com>
2024-03-20 01:44:56 +0600

    admin layoutsetup completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Product from "./pages/Product/Product";
+import Home from "./pages/Home/Home";
+import Cart from "./pages/Cart/Cart";
+import Layout from "./layout/Layout";
 import Authentication from "./pages/Authentication/Authentication";
+import ProtectedRoutes from "./component/HOC/ProtectedtRoutes";
+import ProductDetails from "./pages/ProductDetails/ProductDetails";
+import Admin from "./pages/Admin/Admin";
+import CustomerList from "./pages/Admin/CustomerList/CustomerList";
+import AdminLayout from "./layout/AdminLayout";
+import AddCustomer from "./pages/Admin/CustomerList/AddCustomer";
 
 function App() {
+  // Define a higher-order function to check if the user is signed in
+  const ProtectedLayout = ProtectedRoutes(Layout);
+
   return (
     <>
-      <Authentication />
+      <Routes>
+        <Route path="/" element={<ProtectedLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+          <Route path="/auth" element={<Authentication />} />
+        </Route>
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route path="/admin" element={<Admin />}></Route>
+          <Route path="/admin/customerList" element={<CustomerList />}></Route>
+          <Route path="/admin/add-customer" element={<AddCustomer />}></Route>
+        </Route>
+      </Routes>
     </>
   );
 }
---
+++ src/layout/AdminLayout.tsx
@@ -0,0 +1,170 @@
+import { NavLink, Outlet } from "react-router-dom";
+import Header from "../component/Header/Header";
+import Footer from "../component/Footer/Footer";
+
+const AdminLayout = () => {
+  return (
+    <div>
+      <Header />
+      <div className="container mx-auto">
+        <div className="sticky flex h-screen flex-row gap-4 overflow-y-auto rounded-lg sm:overflow-x-hidden">
+          <aside className="sidebar-sticky sidebar justify-start">
+            <section className="sidebar-title items-center p-4">
+              <svg
+                fill="none"
+                height="42"
+                viewBox="0 0 32 32"
+                width="42"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect height="100%" rx="16" width="100%"></rect>
+                <path
+                  clipRule="evenodd"
+                  d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
+                  fill="currentColor"
+                  fillRule="evenodd"
+                ></path>
+              </svg>
+              <div className="flex flex-col">
+                <span>Acme</span>
+                <span className="text-xs font-normal text-content2">
+                  Team Plan
+                </span>
+              </div>
+            </section>
+            <section className="sidebar-content min-h-[20rem]">
+              <nav className="menu rounded-md">
+                <section className="menu-section px-4">
+                  <span className="menu-title">Main menu</span>
+                  <ul className="menu-items">
+                    <li>
+                      <NavLink to="/admin" className="flex gap-2 menu-item">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-5 w-5 opacity-75"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+                          />
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                          />
+                        </svg>
+                        Admin
+                      </NavLink>
+                    </li>
+
+                    <li>
+                      <input
+                        type="checkbox"
+                        id="menu-2"
+                        className="menu-toggle"
+                      />
+                      <label
+                        className="menu-item justify-between"
+                        htmlFor="menu-2"
+                      >
+                        <div className="flex gap-2">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-5 w-5 opacity-75"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+                            />
+                          </svg>
+                          <span>Customer</span>
+                        </div>
+
+                        <span className="menu-icon">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-5 w-5"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                              clipRule="evenodd"
+                            />
+                          </svg>
+                        </span>
+                      </label>
+
+                      <div className="menu-item-collapse">
+                        <div className="min-h-0">
+                          <label>
+                            <NavLink
+                              to="/admin/customerList"
+                              className="flex gap-2 menu-item ml-6"
+                            >
+                              Customer List
+                            </NavLink>
+                          </label>
+                          <label>
+                            <NavLink
+                              to="/admin/add-customer"
+                              className="flex gap-2 menu-item  ml-6"
+                            >
+                              Add Customer
+                            </NavLink>
+                          </label>
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+                </section>
+              </nav>
+            </section>
+            <section className="sidebar-footer bg-gray-2 pt-2">
+              <div className="divider my-0"></div>
+              <div className="dropdown z-50 flex h-fit w-full cursor-pointer hover:bg-gray-4">
+                <label className="whites mx-2 flex h-fit w-full cursor-pointer p-0 hover:bg-gray-4">
+                  <div className="flex flex-row gap-4 p-4">
+                    <div className="avatar avatar-md">
+                      <img
+                        src="https://i.pravatar.cc/150?img=30"
+                        alt="avatar"
+                      />
+                    </div>
+
+                    <div className="flex flex-col">
+                      <span>Sandra Marx</span>
+                      <span className="text-xs font-normal text-content2">
+                        sandra
+                      </span>
+                    </div>
+                  </div>
+                </label>
+                <div className="dropdown-menu-right-top dropdown-menu ml-2">
+                  <a className="dropdown-item text-sm">Profile</a>
+                </div>
+              </div>
+            </section>
+          </aside>
+          <div className="flex-1 px-5">
+            <Outlet />
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default AdminLayout;
