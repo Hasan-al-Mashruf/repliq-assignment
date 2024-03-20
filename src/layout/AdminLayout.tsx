13f176c1ba1b2903aa34061013af53be459ced15
Moeenuddin Admad <mashruf125@gmail.com>
2024-03-20 16:26:36 +0600

    admin overview is updated

--- src/layout/AdminLayout.tsx
+++ src/layout/AdminLayout.tsx
@@ -58,7 +58,7 @@ const AdminLayout = () => {
                             d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
                           />
                         </svg>
-                        Admin
+                        Overview
                       </NavLink>
                     </li>
 
--- src/pages/Admin/Admin.tsx
+++ src/pages/Admin/Admin.tsx
@@ -1,8 +1,81 @@
+import { useContext } from "react";
+import { cartImg, orderImg, userImg } from "../../assets";
+import {
+  ProductStoreContext,
+  ProductStoreContextType,
+} from "../../context/ProductStoreProvider";
+import { Link } from "react-router-dom";
+
 const Admin = () => {
+  const { orderList, sampleProductData, customerList } = useContext(
+    ProductStoreContext
+  ) as ProductStoreContextType;
   return (
     <div className="container mx-auto">
-      <div className="sticky flex h-screen flex-row gap-4 overflow-y-auto rounded-lg sm:overflow-x-hidden">
-        <div>admin</div>
+      <div className="grid grid-cols-3 gap-3">
+        <div className="card">
+          <div className="card-body">
+            <img src={userImg} alt="" className="w-16" />
+            <div className="flex justify-between items-baseline mt-2">
+              <h2 className="card-header text-xl">Customer</h2>
+              <p className="text-content2 text-sm">
+                Total Customer :{" "}
+                <span className="text-orange-600 font-bold font-title">
+                  {customerList.length.toString().padStart(2, "0")}
+                </span>
+              </p>
+            </div>
+            <div className="card-footer">
+              <Link to="/admin/customerList">
+                <button className="w-full mt-5 btn btn-outline-primary border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white font-body px-8">
+                  Preview All
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-body">
+            <img src={orderImg} alt="" className="w-16" />
+            <div className="flex justify-between items-center mt-2">
+              <h2 className="card-header text-xl">Order</h2>
+              <p className="text-content2 text-sm">
+                Total Order :{" "}
+                <span className="text-orange-600 font-bold font-title">
+                  {orderList.length.toString().padStart(2, "0")}
+                </span>
+              </p>
+            </div>
+            <div className="card-footer">
+              <Link to="/admin/orderList">
+                <button className="w-full mt-5 btn btn-outline-primary border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white font-body px-8">
+                  Preview All
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-body">
+            <img src={cartImg} alt="" className="w-16" />
+            <div className="flex justify-between items-center mt-2">
+              <h2 className="card-header text-xl">Product</h2>
+              <p className="text-content2 text-sm">
+                Total Product :{" "}
+                <span className="text-orange-600 font-bold font-title">
+                  {sampleProductData.length.toString().padStart(2, "0")}
+                </span>
+              </p>
+            </div>
+            <div className="card-footer">
+              <Link to="/admin/productList">
+                <button className="w-full mt-5 btn btn-outline-primary border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white font-body px-8">
+                  Preview All
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
